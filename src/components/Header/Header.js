1b800e061162a672ff194384c81fef588d4cce3a
rkrakibhasan680 <rkrakibhasan680@gmail.com>
2022-09-16 18:44:17 +0600

    json file and table done

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,22 +1,37 @@
-import React from 'react';
-
+import React, { useEffect, useState } from 'react';
+import ProductTable from '../Table/ProductTable';
 const Header = () => {
+const [products,setProducts]=useState([])
+useEffect(()=>{
+    fetch("/products.json")
+    .then(res=>res.json())
+    .then(data=>setProducts(data))
+},[])
+console.log(products,"json products");
+  
     return (
         <div className=''>
-            <div className='container flex justify-between mx-auto'>
-                <div className='header-left flex items-center'>
+            <div className='container flex  justify-between mx-auto shadow-lg py-4 lg:px-0 px-4 rounded-sm '>
+                {/* header right select choice and size */}
+                <div className='header-left flex items-center gap-x-4  pl-5'>
                     <div className='dress-choice'>
-                        <select class="select select-bordered w-full max-w-xs">
+                        <select class="select select-bordered select-sm w-full max-w-xs rounded-sm">
                             <option disabled selected>Choice</option>
-                            <option>Hoodies</option>
-                            <option>Shirt</option>
+                            <option>apparel</option>
+                            <option>footwear</option>
+                            <option>sportswear</option>
+                            <option>traditional</option>
+                            <option>fabric</option>
+                            <option>haute</option>
+
                         </select>
                     </div>
                     <div className='size-choice'>
-                        <select class="select select-bordered w-full max-w-xs">
+                        <select class="select select-bordered select-sm w-full max-w-xs rounded-sm">
                             <option disabled selected>Size</option>
-                            <option>Sm</option>
-                            <option>Lg</option>
+                            <option>S</option>
+                            <option>M</option>
+                            <option>L</option>
                             <option>XL</option>
                         </select>
                     </div>
@@ -24,13 +39,28 @@ const Header = () => {
                         <button>Reset</button>
                     </div>
                 </div>
-                <div className='header-right flex'>
-                
-                        <span> Search: <input type="text" placeholder="Type here" class="input input-bordered  max-w-xs" /></span>
-                    
-                        <button className='btn'>Add to Cart</button>
-                   
+
+                {/* header right search and add to cart part        */}
+                <div className="flex lg:flex-row md:flex-row flex-col gap-x-4 lg:gap-y-0 md:gap-y-0 gap-y-4 pr-5">
+                    {/* search filter */}
+                    <div className="flex items-center gap-x-2">
+                        <span className="font-medium lg:block md:block hidden">Search:</span>
+                        <input
+                            type="text"
+                            placeholder="Enter product name"
+                            className="input input-bordered rounded-sm input-md max-w-xs"
+                        />
+                    </div>
+
+                    {/* add to cart */}
+                    <div>
+                        <button className="btn text-white px-8 bg-[#00a0c0] border-0 rounded-sm">Add to cart</button>
+                    </div>
                 </div>
+            </div>
+
+            <div className='container mx-auto mt-3'>
+                <ProductTable products={products}></ProductTable>
             </div>
         </div>
     );
--- src/components/Table/ProductTable.js
+++ src/components/Table/ProductTable.js
@@ -1,9 +1,131 @@
 import React from 'react';
 
-const ProductTable = () => {
+const ProductTable = ({ products }) => {
     return (
         <div>
-            
+            <div class="overflow-x-auto">
+                <table class="table w-full">
+
+                    <thead>
+                        <tr>
+                            <th>Image</th>
+                            <th>Name</th>
+                            <th>Color</th>
+                            <th>Stock</th>
+                            <th>Price</th>
+                            <th>Buy</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            products.map((product) =>
+                                <tr key={product._id} className="hover">
+                                    <td>
+                                        <img
+                                            src={product.thumbnail}
+                                            alt={product.name}
+                                            className="max-w-full w-16 shadow rounded-md p-2 object-cover"
+                                        />
+                                    </td>
+                                    <td>{product.name}</td>
+                                    <td>{product.color}</td>
+                                    <td>
+                                        {product.stock === "In Stock" ? (
+                                            <span className="text-green-500 flex gap-x-2">
+                                                <span>
+                                                    <svg
+                                                        xmlns="http://www.w3.org/2000/svg"
+                                                        fill="none"
+                                                        viewBox="0 0 24 24"
+                                                        stroke-width="1.5"
+                                                        stroke="currentColor"
+                                                        class="w-6 h-6"
+                                                    >
+                                                        <path
+                                                            stroke-linecap="round"
+                                                            stroke-linejoin="round"
+                                                            d="M15.182 15.182a4.5 4.5 0 01-6.364 0M21 12a9 9 0 11-18 0 9 9 0 0118 0zM9.75 9.75c0 .414-.168.75-.375.75S9 10.164 9 9.75 9.168 9 9.375 9s.375.336.375.75zm-.375 0h.008v.015h-.008V9.75zm5.625 0c0 .414-.168.75-.375.75s-.375-.336-.375-.75.168-.75.375-.75.375.336.375.75zm-.375 0h.008v.015h-.008V9.75z"
+                                                        />
+                                                    </svg>
+                                                </span>
+                                                <span>In Stock</span>
+                                            </span>
+                                        ) : (
+                                            <span className="text-red-500 flex gap-x-2">
+                                                <span>
+                                                    <svg
+                                                        xmlns="http://www.w3.org/2000/svg"
+                                                        fill="none"
+                                                        viewBox="0 0 24 24"
+                                                        stroke-width="1.5"
+                                                        stroke="currentColor"
+                                                        class="w-6 h-6"
+                                                    >
+                                                        <path
+                                                            stroke-linecap="round"
+                                                            stroke-linejoin="round"
+                                                            d="M15.182 16.318A4.486 4.486 0 0012.016 15a4.486 4.486 0 00-3.198 1.318M21 12a9 9 0 11-18 0 9 9 0 0118 0zM9.75 9.75c0 .414-.168.75-.375.75S9 10.164 9 9.75 9.168 9 9.375 9s.375.336.375.75zm-.375 0h.008v.015h-.008V9.75zm5.625 0c0 .414-.168.75-.375.75s-.375-.336-.375-.75.168-.75.375-.75.375.336.375.75zm-.375 0h.008v.015h-.008V9.75z"
+                                                        />
+                                                    </svg>
+                                                </span>
+                                                <span>Out of Stock</span>
+                                            </span>
+                                        )}
+                                    </td>
+                                    <td>
+                                        <span className="flex gap-x-1">
+                                            <span>$</span>
+                                            <span>{product.price}</span>
+                                        </span>
+                                    </td>
+                                    <td>
+                                        <span className="flex gap-x-4 items-center">
+                                            {/* input - cart */}
+                                            <span className="flex gap-x-2 items-center">
+                                                {/* input */}
+                                                <span>
+                                                    <input
+                                                        type="text"
+                                                        placeholder="Type quantity"
+                                                        className="input input-bordered input-sm max-w-xs rounded-sm"
+                                                    />
+                                                </span>
+
+                                                {/* cart */}
+                                                <span className="tooltip" data-tip="Add to Cart">
+                                                    <svg
+                                                        xmlns="http://www.w3.org/2000/svg"
+                                                        fill="none"
+                                                        viewBox="0 0 24 24"
+                                                        stroke-width="1.5"
+                                                        stroke="currentColor"
+                                                        class="w-6 h-6"
+                                                    >
+                                                        <path
+                                                            stroke-linecap="round"
+                                                            stroke-linejoin="round"
+                                                            d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 00-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 00-16.536-1.84M7.5 14.25L5.106 5.272M6 20.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm12.75 0a.75.75 0 11-1.5 0 .75.75 0 011.5 0z"
+                                                        />
+                                                    </svg>
+                                                </span>
+                                            </span>
+
+                                            {/* checkbox */}
+                                            <span className="flex">
+                                                <div className="tooltip text-white" data-tip="Select multiple">
+                                                    <input type="checkbox" className="checkbox checkbox-sm shadow" />
+                                                    {/* <button class="btn">Hover me</button> */}
+                                                </div>
+                                            </span>
+                                        </span>
+                                    </td>
+                                </tr>
+                            )
+                        }
+
+                    </tbody>
+                </table>
+            </div>
         </div>
     );
 };
