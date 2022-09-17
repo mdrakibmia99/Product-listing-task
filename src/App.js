d0129832aee296d668e603e27511e6d5731f36ba
rkrakibhasan680 <rkrakibhasan680@gmail.com>
2022-09-17 21:13:04 +0600

    product checkout left side progress

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Header from './components/Header/Header';
+import ProductCheckOut from './components/ProductCheckOut/ProductCheckOut';
 
 function App() {
   return (
     <div className="">
       <Routes>
         <Route path="/" element={<Header/>} />
+        <Route path="/productCheckOUt" element={<ProductCheckOut/>} />
+        
       </Routes>
     </div>
   );
---
+++ src/components/ProductCheckOut/ProductCheckOut.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+const ProductCheckOut = () => {
+    return (
+        <div>
+
+            <div className="w-full">
+                <div className="overflow-x-auto shadow">
+                    <table className="table w-full">
+                        <thead>
+                            <tr>
+                                <th></th>
+                                <th></th>
+                                <th>Product</th>
+                                <th>Price</th>
+                                <th>Quantity</th>
+                                <th>Subtotal</th>
+                            </tr>
+                        </thead>
+
+                    </table>
+                </div>
+            </div>
+
+        </div>
+    );
+};
+
+export default ProductCheckOut;
