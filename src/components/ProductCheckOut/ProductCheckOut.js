12e905e21a9bcdd3aa61217d57c83f166987fcdc
rkrakibhasan680 <rkrakibhasan680@gmail.com>
2022-09-19 00:09:50 +0600

    checkout page in progress again

--- src/components/ProductCheckOut/ProductCheckOut.js
+++ src/components/ProductCheckOut/ProductCheckOut.js
@@ -1,14 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import CheckOutLeft from './CheckOutLeft';
 import CheckOutRight from './CheckOutRight';
 
 const ProductCheckOut = () => {
+  const [deleteProduct,setDeleteProduct]=useState(0)
     return (
         <section className="py-4 lg:px-0 md:px-0 px-4">
         <div className="container mx-auto">
           <div className="flex lg:flex-row flex-col justify-between lg:gap-x-4 gap-y-4">
-            <CheckOutLeft/>
-            <CheckOutRight/>
+            <CheckOutLeft setDeleteProduct={setDeleteProduct} deleteProduct={deleteProduct}/>
+            <CheckOutRight deleteProduct={deleteProduct}/>
           </div>
         </div>
       </section>
