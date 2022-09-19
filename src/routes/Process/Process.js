9d2ccab2aea1020901ae65de3186444f4be9ac55
rkrakibhasan680 <rkrakibhasan680@gmail.com>
2022-09-20 00:09:59 +0600

    after procces work done

--- src/routes/Process/Process.js
+++ src/routes/Process/Process.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const Process = () => {
+    const navigate = useNavigate();
     return (
-        <div>
-            <h1>Thank you!!</h1>
+        <div className='flex flex-col justify-center items-center min-h-screen'>
+            <h1 className='text-5xl font-bold '>Thank you!!</h1>
+            <button
+            onClick={()=>navigate("/home")}
+             className="btn text-white lg:px-8 md:px-8 px-3 bg-[#00a0c0] border-0  whitespace-nowrap mt-5 rounded-md"> Home</button>
         </div>
     );
 };
